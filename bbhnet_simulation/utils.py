d4921e2b95429bb841196c36096929fe8ed38510
Tri Nguyen <tnguy@mit.edu>
2021-11-29 21:32:36 -0500

    finish up injection script

--- bbhnet_simulation/utils.py
+++ bbhnet_simulation/utils.py
@@ -84,6 +84,80 @@ def time_domain_blip(duration, sample_rate, f0_min=20, f0_width=1000, N=20):
         x += time_domain_sine_gaussian(params, duration, sample_rate)
     return x
 
+def simulate_bbh_signals(
+    sample_params, sample_rate, sample_duration, triggers, H1_psd, L1_psd,
+    flow=None, fhigh=None):
+    ''' generate BBH signals
+    Arguments:
+    - sample_params: dictionary of GW parameters
+    - sample_duration: time duration of each sample
+    - triggers: trigger time (relative to `sample_duration`) of each sample
+    - H1_psd, L1_psd: Hanford and Livingston PSD
+    '''
+
+    # define some signal properties
+    # get total number of samples
+    waveform_duration = 8
+    num_samples = len(sample_params['geocent_time'])
+    sample_size = int(sample_rate * sample_duration)
+    waveform_size = int(sample_rate * waveform_duration)
+
+    # define a Bilby waveform generator
+    waveform_generator = bilby.gw.WaveformGenerator(
+        duration=waveform_duration, sampling_frequency=sample_rate,
+        frequency_domain_source_model=lal_binary_black_hole,
+        parameter_conversion=convert_to_lal_binary_black_hole_parameters,
+        waveform_arguments={
+            'waveform_approximant': 'IMRPhenomPv2',
+            'reference_frequency': 50,
+            'minimum_frequency': 20})
+
+    # loop over all BBH signals
+    signals = np.zeros((num_samples, 2, sample_size))
+    snr = np.zeros((num_samples, 2))
+    for i in range(num_samples):
+        # get parameters of the current BBH signal
+        p = dict()
+        for k, v in sample_params.items():
+            p[k] = v[i]
+        ra, dec, geocent_time, psi = p['ra'], p['dec'], p['geocent_time'], p['psi']
+        polarizations = waveform_generator.time_domain_strain(p)
+
+        # simulate signals for Hanford and Livingston
+        for j, ifo_name in enumerate(('H1', 'L1')):
+            # get detector and PSD
+            if ifo_name == 'H1':
+                noise_psd = H1_psd
+            else:
+                noise_psd = L1_psd
+            ifo = bilby.gw.detector.get_empty_interferometer(ifo_name)
+
+            # add polarizations with detector geometry
+            signal = np.zeros(waveform_size)
+            for mode in polarizations.keys():
+                # Get H1 response
+                response = ifo.antenna_response(ra, dec, geocent_time, psi, mode)
+                signal += polarizations[mode] * response
+
+            # shift signal back to the trigger time of the simulated strain
+            # also add shift due to time travel between detectors
+            # total shift = shift to trigger time + geometric shift
+            dt = (waveform_duration - sample_duration) / 2. + triggers[i]
+            dt += ifo.time_delay_from_geocenter(ra, dec, geocent_time)
+            signal = np.roll(signal, int(np.round(dt*sample_rate)))
+
+            # calculate snr
+            snr[i][j] = get_snr(signal, noise_psd, sample_rate)
+
+            # truncate signal
+            istart = (waveform_size - sample_size) // 2
+            istop = istart + sample_size
+            signal = signal[istart:istop]
+
+            signals[i, j] += signal
+
+    return signals, snr
+
 def simulate_whitened_bbh_signals(
     sample_params, sample_rate, sample_duration, triggers, H1_psd, L1_psd,
     flow=None, fhigh=None):
--- inject_waveforms.py
+++ inject_waveforms.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import h5py
+import copy
 import argparse
 import logging
 
@@ -33,6 +34,8 @@ def parse_cmd():
                         help='path to write Hanford output file in GWF format')
     parser.add_argument('-oL', '--L1-outfile', required=True,
                         help='path to write Livingston output file in GWF format')
+    parser.add_argument('-oP', '--parameters-outfile', required=True,
+                        help='path to write output file containing parameters')
 
     # signal simulation args
     parser.add_argument(
@@ -41,12 +44,14 @@ def parse_cmd():
     parser.add_argument(
         '-p', '--prior-file', required=True,
         help='path to prior config file. Required for signal simulation')
-    parser.add_argument(
-        '--min-trigger', type=float, default=0.05,
-        help='mininum trigger time w.r.t to sample. must be within [0, sample_duration]')
-    parser.add_argument(
-        '--max-trigger', type=float, default=0.95,
-        help='maximum trigger time w.r.t to sample. must be within [0, sample_duration]')
+    parser.add_argument('--min-dt', type=float, default=20,
+                        help='Minimum time between signals')
+    parser.add_argument('--max-dt', type=float, default=100,
+                        help='Maximum time between signals')
+    parser.add_argument('--min-snr', type=float, default=25,
+                        help='mininum signal-to-noise ratio')
+    parser.add_argument('--max-snr', type=float, default=25,
+                        help='maximum signal-to-noise ratio') 
 
     return parser.parse_args()
 
@@ -56,27 +61,36 @@ if __name__ == '__main__':
 
     # parse command-line arguments
     FLAGS = parse_cmd()
-
-    # compute some sim parameters from cmd input
-    sample_size = int(FLAGS.sample_rate * FLAGS.sample_duration)
-    fftlength = int(max(2, np.ceil(2048 / FLAGS.sample_rate)))
+    waveform_duration = 8
 
     # read input frame file and get attributes
+    logging.info('Reading Hanford    data from : {}'.format(FLAGS.H1_infile))
+    logging.info('Reading Livingston data from : {}'.format(FLAGS.L1_infile))
+
     H1_strain = TimeSeries.read(FLAGS.H1_infile, 'H1:GWOSC-4KHZ_R1_STRAIN')
-    L1_strain = TimeSeries.read(FLAGS.L1_infile, 'L1:GWOSC-4KHZ_R1_STRAIN')
+    L1_strain = TimeSeries.read(FLAGS.L1_infile, 'H1:GWOSC-4KHZ_R1_STRAIN')
     sample_rate = H1_strain.sample_rate.value
     duration = H1_strain.duration.value
-    fftlength = int(max(2, np.ceil(2048 / sample_rate))
-
-    print(H1_strain, L1_strain)
-    print(sample_rate, duration)
+    fftlength = int(max(2, np.ceil(2048 / sample_rate)))
 
     # calculate strain psd
     H1_psd = H1_strain.psd(fftlength)
     L1_psd = L1_strain.psd(fftlength)
 
-    # simulate waveform
-    times = np.arange() + H1.t0.value + waveform_duration // 2
+    # Simulate waveform
+    # first, we sample the time between subsequent signals
+    max_num_samples = int(duration // FLAGS.min_dt)
+    times = np.random.uniform(FLAGS.min_dt, FLAGS.max_dt, max_num_samples)
+    times = np.cumsum(times)
+    times = times[times < duration - FLAGS.min_dt]
+    times += H1_strain.t0.value
+    num_samples = len(times)
+
+    logging.info('Number of samples: {}'.format(num_samples))
+    logging.info('Minimum time between signals: {} s'.format(FLAGS.min_dt))
+    logging.info('Maximum time between signals: {} s'.format(FLAGS.max_dt))
+    logging.info('Minimum SNR: {}'.format(FLAGS.min_snr))
+    logging.info('Maximum SNR: {}'.format(FLAGS.max_snr))
 
     if FLAGS.signal_type == 'bbh':
         # sample GW parameters from a prior distribution
@@ -85,9 +99,56 @@ if __name__ == '__main__':
         sample_params = priors.sample(num_samples)
 
         # Bilby does not sample GPS time, so we have to manually do it
-        triggers = np.zeros(FLAGS.n_sample) * triggers
-        sample_params['geocent_time'] = triggers + times
+        triggers = np.zeros(num_samples) + waveform_duration // 2
+        sample_params['geocent_time'] = times
 
         # simulate whitened GW waveforms
-        signals, snr = utils.simulate_whitened_bbh_signals(
-            sample_params, FLAGS.sample_rate, FLAGS.sample_duration, triggers)
+        signals, old_snr = utils.simulate_bbh_signals(
+            sample_params, sample_rate, waveform_duration, triggers,
+            H1_psd=H1_psd, L1_psd=L1_psd)
+        
+        # sample snr
+        new_snr = np.random.uniform(FLAGS.min_snr, FLAGS.max_snr, num_samples)
+
+        # scale signal to new snr
+        scale = new_snr / np.sqrt(np.sum(old_snr**2, axis=1))
+        signals = signals * scale.reshape(-1, 1, 1)
+        snr = old_snr * scale.reshape(-1, 1)
+        sample_params['luminosity_distance'] /= scale
+
+    # write to a different frame
+    logging.info('Writing new Hanford    data to: {}'.format(FLAGS.H1_outfile))
+    logging.info('Writing new Livingston data to: {}'.format(FLAGS.L1_outfile))
+    
+    # create a copy of current strain
+    # add signals into new strain
+    H1_new_strain = copy.deepcopy(H1_strain)
+    L1_new_strain = copy.deepcopy(L1_strain)
+    for t, signal in zip(times, signals):
+        H1_signal = signal[0]
+        L1_signal = signal[1]
+       
+        tstart_offset = t - H1_strain.t0.value - waveform_duration // 2
+        istart = int(sample_rate * tstart_offset)
+        istop = istart + int(sample_rate * waveform_duration)
+        H1_new_strain[istart: istop] += H1_signal * H1_new_strain.unit
+        L1_new_strain[istart: istop] += L1_signal * L1_new_strain.unit
+
+    H1_new_strain.write(FLAGS.H1_outfile)
+    L1_new_strain.write(FLAGS.L1_outfile)
+
+    # write output parameters
+    logging.info('Write output parameters to {}'.format(
+        FLAGS.parameters_outfile))
+    with h5py.File(FLAGS.parameters_outfile, 'w') as f:
+        f.create_dataset('GPS-start', data=times)
+        f.create_group('H1')
+        f['H1'].create_dataset('SNR', data=snr[:, 0])
+        f['H1'].create_dataset('signal', data=signals[:, 0])
+        f.create_group('L1')
+        f['L1'].create_dataset('SNR', data=snr[:, 1])
+        f['L1'].create_dataset('signal', data=signals[:, 1])
+        
+        f.create_group('signal_params')
+        for k, v in sample_params.items():
+            f['signal_params'].create_dataset(k, data=v)
